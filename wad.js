e760d6a3926923a18a6baafb715a35f4156644e0
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2026-02-14 09:29:19 -0500

    parsing map from wad, untested

--- wad.js
+++ wad.js
@@ -41,11 +41,104 @@ class Texture
     }
 }
 
-class World
+class Linedef
 {
-
+    constructor()
+    {
+        this.v1 = null;
+        this.v2 = null;
+        this.frontside = null;
+        this.backside = null;
+    }
 }
 
+class Sidedef
+{
+    constructor()
+    {
+        this.xoffs = null;
+        this.yoffs = null;
+        this.upper = null;
+        this.lower = null;
+        this.middle = null;
+        this.sector = null;
+    }
+}
+
+class Vertex
+{
+    constructor(x, y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Seg
+{
+    constructor()
+    {
+        this.v1 = null;
+        this.v2 = null;
+    }
+}
+
+class SSector
+{
+    constructor()
+    {
+        this.nseg = null;
+        this.firstseg = null;
+    }
+}
+
+class Node
+{
+    constructor()
+    {
+        this.x = null;
+        this.y = null;
+        this.dx = null;
+        this.dy = null;
+        
+        // (right, left)
+        this.xmins = [ null, null, ];
+        this.ymins = [ null, null, ];
+        this.xmaxs = [ null, null, ];
+        this.ymaxs = [ null, null, ];
+
+        this.children = [ null, null, ];
+    }
+}
+
+class Sector
+{
+    constructor()
+    {
+        this.floor = null;
+        this.ceil = null;
+        this.floortex = null;
+        this.ceiltex = null;
+    }
+}
+
+class World
+{
+    constructor()
+    {
+        this.name = null;
+        this.linedefs = [];
+        this.sidedefs = [];
+        this.vertices = [];
+        this.segs = [];
+        this.ssectors = [];
+        this.nodes = [];
+        this.sectors = [];
+    }
+}
+
+// World
+export var maps = new Map();
 export var textures = new Map();
 export var palettes = [];
 export var patches = new Map(); // name-indexed
@@ -203,40 +296,229 @@ function processpnames(data, loc, size, name)
 
 var inpatches = false;
 var maplumpcounter = 0;
-var curmapname = null;
 
 var curmap = null;
 
-function processmaplump(data, headerloc)
+function processlinedefs(data, name, loc, size)
+{
+    if(name != "LINEDEFS")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order LINEDEFS lump');
+
+    const nlinedefs = size / 14;
+    curmap.linedefs.length = nlinedefs;
+
+    for(let i=0; i<nlinedefs; i++)
+    {
+        const lineloc = loc + i * 14;
+
+        let line = new Linedef();
+
+        line.v1 = data.getInt16(lineloc, true);
+        line.v2 = data.getInt16(lineloc + 2, true);
+        line.frontside = data.getInt16(lineloc + 10, true);
+        line.backside = data.getInt16(lineloc + 12, true);
+
+        curmap.linedefs[i] = line;
+    }
+}
+
+function parsestring8(data, loc)
+{
+    let str = "";
+    for (let i=0; i<8; i++)
+    {
+        const char = data.getUint8(loc + 8 + i);
+        if (char === 0) break;
+        str += String.fromCharCode(char);
+    }
+    return str.trim();
+}
+
+function processsidedefs(data, name, loc, size)
+{
+    if(name != "SIDEDEFS")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order SIDEDEFS lump');
+
+    const nsidedefs = size / 30;
+    curmap.sidedefs.length = nsidedefs;
+
+    for(let i=0; i<nsidedefs; i++)
+    {
+        const sideloc = loc + i * 30;
+
+        let side = new Sidedef();
+
+        side.xoffs = data.getInt16(sideloc, true);
+        side.yoffs = data.getInt16(sideloc + 2, true);
+        side.upper = parsestring8(data, sideloc + 4);
+        side.lower = parsestring8(data, sideloc + 12);
+        side.middle = parsestring8(data, sideloc + 20);
+        side.sector = data.getInt16(sideloc + 28, true);
+
+        curmap.sidedefs[i] = side;
+    }
+}
+
+function processvertices(data, name, loc, size)
+{
+    if(name != "VERTEXES")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order VERTEXES lump');
+
+    const nverts = size / 4;
+    curmap.vertices.length = nverts;
+
+    for(let i=0; i<nverts; i++)
+    {
+        const vertloc = loc + i * 4;
+
+        let vert = new Vertex();
+
+        vert.x = data.getInt16(vertloc, true);
+        vert.y = data.getInt16(vertloc + 2, true);
+
+        curmap.vertices[i] = vert;
+    }
+}
+
+function processsegs(data, name, loc, size)
+{
+    if(name != "SEGS")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order SEGS lump');
+
+    const nsegs = size / 12;
+    curmap.segs.length = nverts;
+
+    for(let i=0; i<nsegs; i++)
+    {
+        const segloc = loc + i * 12;
+
+        let seg = new Seg();
+
+        seg.v1 = data.getInt16(segloc, true);
+        seg.v2 = data.getInt16(segloc + 2, true);
+
+        curmap.segs[i] = seg;
+    }
+}
+
+function processssectors(data, name, loc, size)
+{
+    if(name != "SSECTORS")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order SSECTORS lump');
+
+    const nssectors = size / 4;
+    curmap.ssectors.length = nssectors;
+
+    for(let i=0; i<nssectors; i++)
+    {
+        const sectorloc = loc + i * 4;
+
+        let sector = new SSector();
+
+        sector.nseg = data.getInt16(segloc, true);
+        sector.firstseg = data.getInt16(segloc + 2, true);
+
+        curmap.ssectors[i] = sector;
+    }
+}
+
+function processnodes(data, name, loc, size)
+{
+    if(name != "NODES")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order NODES lump');
+
+    const nnodes = size / 28;
+    curmap.nodes.length = nnodes;
+
+    for(let i=0; i<nnodes; i++)
+    {
+        const nodeloc = loc + i * 28;
+
+        let node = new Node();
+
+        node.x = data.getInt16(segloc, true);
+        node.y = data.getInt16(segloc + 2, true);
+        node.dx = data.getInt16(segloc + 4, true);
+        node.dy = data.getInt16(segloc + 6, true);
+
+        node.ymaxs[0] = data.getInt16(segloc + 8, true);
+        node.ymins[0] = data.getInt16(segloc + 10, true);
+        node.xmaxs[0] = data.getInt16(segloc + 12, true);
+        node.xmins[0] = data.getInt16(segloc + 14, true);
+        node.ymaxs[1] = data.getInt16(segloc + 16, true);
+        node.ymins[1] = data.getInt16(segloc + 18, true);
+        node.xmaxs[1] = data.getInt16(segloc + 20, true);
+        node.xmins[1] = data.getInt16(segloc + 22, true);
+        
+        node.children[0] = data.getInt16(segloc + 24, true);
+        node.children[1] = data.getInt16(segloc + 26, true);
+
+        curmap.nodes[i] = node;
+    }
+}
+
+function processsectors(data, name, loc, size)
+{
+    if(name != "SECTORS")
+        throw new Error('map ' + curmap.name + ' lacks or has out of order SECTORS lump');
+
+    const nsectors = size / 26;
+    curmap.sectors.length = nsectors;
+
+    for(let i=0; i<nssectors; i++)
+    {
+        const sectorloc = loc + i * 26;
+
+        let sector = new Sector();
+
+        sector.floor = data.getInt16(sectorloc, true);
+        sector.ceil = data.getInt16(sectorloc + 2, true);
+        sector.floortex = parsestring8(data, sectorloc + 4);
+        sector.ceiltex = parsestring8(data, sectorloc + 12);
+
+        curmap.sectors[i] = sector;
+    }
+}
+
+function processmaplump(data, name, loc, size)
 {
     switch(maplumpcounter)
     {
     case 1:
         break;
     case 2:
+        processlinedefs(data, name, loc, size);
         break;
     case 3:
+        processsidedefs(data, name, loc, size)
         break;
     case 4:
+        processvertices(data, name, loc, size)
         break;
     case 5:
+        processsegs(data, name, loc, size);
         break;
     case 6:
+        processssectors(data, name, loc, size);
         break;
     case 7:
+        processnodes(data, name, loc, size);
         break;
     case 8:
+        processsectors(data, name, loc, size);
         break;
     case 9:
         break;
     case 10:
         break;
     default:
+        break;
     }
 
     maplumpcounter++;
     if(maplumpcounter > 10)
     {
+        maps.set(curmap.name, curmap);
         maplumpcounter = 0;
         curmapname = null;
     }
@@ -257,11 +539,11 @@ function processlumpheader(data, headerloc)
     name = name.trim();
 
     if(maplumpcounter != 0)
-        processmaplump(data, headerloc);
+        processmaplump(data, name, loc, size);
     else if(/^E\dM\d$/.test(name))
     {
-        curmap = new Map();
-        curmapname = name;
+        curmap = new World();
+        curmap.name = name;
     }
     else if(name == "P_START")
         inpatches = true;
